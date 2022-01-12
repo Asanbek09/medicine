51324ad7689cdc9ed3d717c9b2d17bb6997db8e4
Asanbek Samudin uulu <samudinua@gmail.com>
2022-01-12 17:34:50 +0600

    с добавлены функции в views

--- api/clients/urls.py
+++ api/clients/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('all/', views.clientsList, name='all'),
+    path('create/', views.createClient, name='create')
 ]
--- api/clients/views.py
+++ api/clients/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from api.clients.models import Client
+from api.clients.serializers import ClientSerializer
+
+
 @api_view(['GET'])
 def index(request):
-  api_urls = {
-    'list': '/all/',
-    'create': '/create/'
-  }
-  return Response(api_urls)
+    api_urls = {
+        'list': '/all/',
+        'create': '/create/'
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def clientsList(request):
+    clients = Client.objects.all().order_by('-id')
+    serializer = ClientSerializer(clients, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createClient(request):
+    serializer = ClientSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
