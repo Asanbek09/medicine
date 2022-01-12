a6a4a6b087b115f2fa5b0e656f47ddc66abd5ed8
Asanbek Samudin uulu <samudinua@gmail.com>
2022-01-12 17:22:42 +0600

    создан модель Client

--- api/clients/models.py
+++ api/clients/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    date_of_birth = models.DateField(
+        auto_now_add=False, null=True, blank=True)
+    diagnoses = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
